599925225528278eb399d054966562c7d8b775fa
Helen Zhu <lovelyhelenzhu@gmail.com>
2017-02-01 20:55:05 -0500

    1st Step

--- Hello Swift/ViewController.swift
+++ Hello Swift/ViewController.swift
@@ -10,9 +10,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var namelala: UILabel!
+    
+
+    
+    @IBAction func Button1(_ sender: Any) {
+        namelala.text = "哼哼哼"
+    }
+    
+    
+    
+    @IBAction func Button2(_ sender: Any) {
+        namelala.text = "啊啊啊啊啊！！！！！"
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.view.backgroundColor = UIColor.red
+        
     }
 
     override func didReceiveMemoryWarning() {
